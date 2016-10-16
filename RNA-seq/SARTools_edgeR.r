cebf0dfae46754bdd18806e5c79fc7118697681c
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2016-10-16 17:04:32 +0800

    Add files via upload

---
+++ RNA-seq/SARTools_edgeR.r
@@ -0,0 +1,100 @@
+################################################################################
+### R script to compare several conditions with the SARTools and edgeR packages
+### Hugo Varet
+### May 9th, 2016
+### designed to be executed with SARTools 1.3.2
+################################################################################
+
+################################################################################
+###                parameters: to be modified by the user                    ###
+################################################################################
+
+sink( file="runLog.txt")
+
+dir1 <- "2B-edgeR"
+if( ! file.exists(dir1) ) { dir.create(path=dir1,   recursive = TRUE) }
+
+
+rm(list=ls())                                        # remove all the objects from the R session
+
+workDir     <- "."                                   # working directory for the R session
+projectName <- "RAN-seq"                             # name of the project
+author      <- "Yong Peng"                           # author of the statistical analysis/report
+
+targetFile <- "target.txt"                           # path to the design/target file
+rawDir     <- "1-rawCounts"                          # path to the directory containing raw counts files
+featuresToRemove <- c("alignment_not_unique",        # names of the features to be removed
+                      "ambiguous", "no_feature",     # (specific HTSeq-count information and rRNA for example)
+                      "not_aligned", "too_low_aQual")# NULL if no feature to remove
+
+varInt <- "condition"                                # factor of interest
+condRef <- "WT"                                      # reference biological condition
+batch <- NULL                                        # blocking factor: NULL (default) or "batch" for example
+
+alpha <- 0.05                                        # threshold of statistical significance
+pAdjustMethod <- "BH"                                # p-value adjustment method: "BH" (default) or "BY"
+
+cpmCutoff <- 1                                       # counts-per-million cut-off to filter low counts
+gene.selection <- "pairwise"                         # selection of the features in MDSPlot
+normalizationMethod <- "TMM"                         # normalization method: "TMM" (default), "RLE" (DESeq) or "upperquartile"
+
+colors <- c("dodgerblue","firebrick1",               # vector of colors of each biological condition on the plots
+            "MediumVioletRed","SpringGreen")
+
+################################################################################
+###                             running script                               ###
+################################################################################
+setwd(workDir)
+library(SARTools)
+
+# checking parameters
+checkParameters.edgeR(projectName=projectName,author=author,targetFile=targetFile,
+                      rawDir=rawDir,featuresToRemove=featuresToRemove,varInt=varInt,
+                      condRef=condRef,batch=batch,alpha=alpha,pAdjustMethod=pAdjustMethod,
+                      cpmCutoff=cpmCutoff,gene.selection=gene.selection,
+                      normalizationMethod=normalizationMethod,colors=colors)
+
+# loading target file
+target <- loadTargetFile(targetFile=targetFile, varInt=varInt, condRef=condRef, batch=batch)
+
+# loading counts
+counts <- loadCountData(target=target, rawDir=rawDir, featuresToRemove=featuresToRemove)
+
+# description plots
+majSequences <- descriptionPlots(counts=counts, group=target[,varInt], col=colors)
+
+# edgeR analysis
+out.edgeR <- run.edgeR(counts=counts, target=target, varInt=varInt, condRef=condRef,
+                       batch=batch, cpmCutoff=cpmCutoff, normalizationMethod=normalizationMethod,
+                       pAdjustMethod=pAdjustMethod)
+
+# MDS + clustering
+exploreCounts(object=out.edgeR$dge, group=target[,varInt], gene.selection=gene.selection, col=colors)
+
+# summary of the analysis (boxplots, dispersions, export table, nDiffTotal, histograms, MA plot)
+summaryResults <- summarizeResults.edgeR(out.edgeR, group=target[,varInt], counts=counts, alpha=alpha, col=colors)
+
+# save image of the R session
+save.image(file=paste0(projectName, ".RData"))
+
+# generating HTML report
+writeReport.edgeR(target=target, counts=counts, out.edgeR=out.edgeR, summaryResults=summaryResults,
+                  majSequences=majSequences, workDir=workDir, projectName=projectName, author=author,
+                  targetFile=targetFile, rawDir=rawDir, featuresToRemove=featuresToRemove, varInt=varInt,
+                  condRef=condRef, batch=batch, alpha=alpha, pAdjustMethod=pAdjustMethod, colors=colors,
+                  gene.selection=gene.selection, normalizationMethod=normalizationMethod)
+
+
+
+sink()
+
+
+sink( file="versions.txt") 
+sessionInfo()
+sink()
+
+
+
+
+
+
