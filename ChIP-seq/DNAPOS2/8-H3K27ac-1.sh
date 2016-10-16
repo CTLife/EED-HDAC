d13f8a3b5391c4895dd01105948ccc0161b4c88f
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2016-10-16 17:00:12 +0800

    Add files via upload

---
+++ ChIP-seq/DNAPOS2/8-H3K27ac-1.sh
@@ -0,0 +1,18 @@
+
+
+mkdir  -p     6-Profile/8-H3K27ac/broadPeak_H3K27ac-EEDko-AdultCM-spe
+danpos.py    profile   4-NorE7/H3K27ac-lib1655-EEDhete-control-adultCMs_Rep1.wig     --bed3file_paths 5-DNAregions/8-H3K27ac/broadPeak_H3K27ac-EEDko-AdultCM-spe   --heatmap  1    --name  6-Profile/8-H3K27ac/broadPeak_H3K27ac-EEDko-AdultCM-spe/lib1655-EEDhete-control_Rep1      --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 1000    --plot_row 1     --plot_column 1    --plot_colors  green                                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1656-EEDhete-control-adultCMs_Rep2.wig     --bed3file_paths 5-DNAregions/8-H3K27ac/broadPeak_H3K27ac-EEDko-AdultCM-spe   --heatmap  1    --name  6-Profile/8-H3K27ac/broadPeak_H3K27ac-EEDko-AdultCM-spe/lib1656-EEDhete-control_Rep2      --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 1000    --plot_row 1     --plot_column 1    --plot_colors  green                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1657-EEDko-AAVHDAC-adultCMs_Rep1.wig       --bed3file_paths 5-DNAregions/8-H3K27ac/broadPeak_H3K27ac-EEDko-AdultCM-spe   --heatmap  1    --name  6-Profile/8-H3K27ac/broadPeak_H3K27ac-EEDko-AdultCM-spe/lib1657-EEDko-AAVHDAC_Rep1        --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 1000    --plot_row 1     --plot_column 1    --plot_colors  green                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1658-EEDko-AAVluci-adultCMs_Rep1.wig       --bed3file_paths 5-DNAregions/8-H3K27ac/broadPeak_H3K27ac-EEDko-AdultCM-spe   --heatmap  1    --name  6-Profile/8-H3K27ac/broadPeak_H3K27ac-EEDko-AdultCM-spe/lib1658-EEDko-AAVluci_Rep1        --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 1000    --plot_row 1     --plot_column 1    --plot_colors  green                                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1659-EEDko-AAVluci-adultCMs_Rep2.wig       --bed3file_paths 5-DNAregions/8-H3K27ac/broadPeak_H3K27ac-EEDko-AdultCM-spe   --heatmap  1    --name  6-Profile/8-H3K27ac/broadPeak_H3K27ac-EEDko-AdultCM-spe/lib1659-EEDko-AAVluci_Rep2        --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 1000    --plot_row 1     --plot_column 1    --plot_colors  green                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1660-EEDko-AAVHDAC-adultCMs_Rep2.wig       --bed3file_paths 5-DNAregions/8-H3K27ac/broadPeak_H3K27ac-EEDko-AdultCM-spe   --heatmap  1    --name  6-Profile/8-H3K27ac/broadPeak_H3K27ac-EEDko-AdultCM-spe/lib1660-EEDko-AAVHDAC_Rep2        --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 1000    --plot_row 1     --plot_column 1    --plot_colors  green                  
+
+
+
+
+
+
+
+
+
---
+++ ChIP-seq/DNAPOS2/rescueDEGs_EED_FC1.5.sh
@@ -0,0 +1,32 @@
+
+
+
+mkdir  -p     6-Profile/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.up
+danpos.py    profile   4-NorE7/H3K27ac-lib1655-EEDhete-control-adultCMs_Rep1.wig     --genefile_paths 5-DNAregions/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.up   --genomic_sites  TSS   --heatmap  1    --name  6-Profile/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.up/lib1655-EEDhete-control_Rep1      --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 10000    --plot_row 1     --plot_column 1    --plot_colors  green                                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1656-EEDhete-control-adultCMs_Rep2.wig     --genefile_paths 5-DNAregions/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.up   --genomic_sites  TSS   --heatmap  1    --name  6-Profile/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.up/lib1656-EEDhete-control_Rep2      --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 10000    --plot_row 1     --plot_column 1    --plot_colors  green                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1657-EEDko-AAVHDAC-adultCMs_Rep1.wig       --genefile_paths 5-DNAregions/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.up   --genomic_sites  TSS   --heatmap  1    --name  6-Profile/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.up/lib1657-EEDko-AAVHDAC_Rep1        --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 10000    --plot_row 1     --plot_column 1    --plot_colors  green                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1658-EEDko-AAVluci-adultCMs_Rep1.wig       --genefile_paths 5-DNAregions/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.up   --genomic_sites  TSS   --heatmap  1    --name  6-Profile/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.up/lib1658-EEDko-AAVluci_Rep1        --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 10000    --plot_row 1     --plot_column 1    --plot_colors  green                                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1659-EEDko-AAVluci-adultCMs_Rep2.wig       --genefile_paths 5-DNAregions/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.up   --genomic_sites  TSS   --heatmap  1    --name  6-Profile/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.up/lib1659-EEDko-AAVluci_Rep2        --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 10000    --plot_row 1     --plot_column 1    --plot_colors  green                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1660-EEDko-AAVHDAC-adultCMs_Rep2.wig       --genefile_paths 5-DNAregions/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.up   --genomic_sites  TSS   --heatmap  1    --name  6-Profile/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.up/lib1660-EEDko-AAVHDAC_Rep2        --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 10000    --plot_row 1     --plot_column 1    --plot_colors  green                  
+
+
+
+
+
+mkdir  -p     6-Profile/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.down
+danpos.py    profile   4-NorE7/H3K27ac-lib1655-EEDhete-control-adultCMs_Rep1.wig     --genefile_paths 5-DNAregions/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.down   --genomic_sites  TSS   --heatmap  1    --name  6-Profile/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.down/lib1655-EEDhete-control_Rep1      --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 10000    --plot_row 1     --plot_column 1    --plot_colors  green                                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1656-EEDhete-control-adultCMs_Rep2.wig     --genefile_paths 5-DNAregions/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.down   --genomic_sites  TSS   --heatmap  1    --name  6-Profile/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.down/lib1656-EEDhete-control_Rep2      --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 10000    --plot_row 1     --plot_column 1    --plot_colors  green                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1657-EEDko-AAVHDAC-adultCMs_Rep1.wig       --genefile_paths 5-DNAregions/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.down   --genomic_sites  TSS   --heatmap  1    --name  6-Profile/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.down/lib1657-EEDko-AAVHDAC_Rep1        --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 10000    --plot_row 1     --plot_column 1    --plot_colors  green                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1658-EEDko-AAVluci-adultCMs_Rep1.wig       --genefile_paths 5-DNAregions/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.down   --genomic_sites  TSS   --heatmap  1    --name  6-Profile/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.down/lib1658-EEDko-AAVluci_Rep1        --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 10000    --plot_row 1     --plot_column 1    --plot_colors  green                                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1659-EEDko-AAVluci-adultCMs_Rep2.wig       --genefile_paths 5-DNAregions/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.down   --genomic_sites  TSS   --heatmap  1    --name  6-Profile/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.down/lib1659-EEDko-AAVluci_Rep2        --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 10000    --plot_row 1     --plot_column 1    --plot_colors  green                  
+danpos.py    profile   4-NorE7/H3K27ac-lib1660-EEDko-AAVHDAC-adultCMs_Rep2.wig       --genefile_paths 5-DNAregions/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.down   --genomic_sites  TSS   --heatmap  1    --name  6-Profile/rescueDEGs_EED_FC1.5/EEDkoAAVlucivsEEDhete.down/lib1660-EEDko-AAVHDAC_Rep2        --bin_size 20   --flank_up 5000    --flank_dn 5000    --region_size 10000    --plot_row 1     --plot_column 1    --plot_colors  green                  
+
+
+
+
+
+
+
+
+
+
